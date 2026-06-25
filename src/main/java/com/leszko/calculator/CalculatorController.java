cf556c3cf9ef222144f170fef2c0c209dd2b324f
yakob <ydima169@gmail.com>
2026-06-25 17:55:14 +0700

    Fix: correct JAR filename in Dockerfile, add CORS support

--- src/main/java/com/leszko/calculator/CalculatorController.java
+++ src/main/java/com/leszko/calculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package com.leszko.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 class CalculatorController {
 
